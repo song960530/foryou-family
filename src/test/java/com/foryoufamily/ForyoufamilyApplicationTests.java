dba653a047b187f3aafb6bbd6664aed6e2fd0011
송문준 <song960530@naver.com>
2022-06-21 13:40:02 +0900

    init: 프로젝트 초기 생성

---
+++ src/test/java/com/foryoufamily/ForyoufamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foryoufamily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForyoufamilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
