12adc7791a51efeee0ba29a78a8f9ab836efc0c1
송문준 <song960530@naver.com>
2022-06-22 21:42:28 +0900

    docs: 에러코드 주석 추가

--- src/main/java/com/foryoufamily/global/error/ErrorCode.java
+++ src/main/java/com/foryoufamily/global/error/ErrorCode.java
@@ -8,13 +8,13 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    /* 400  */
+    /* 400 BAD_REQUEST: 파라미터가 잘못 넘어옴 */
     ARGUMENT_NOT_VALID(HttpStatus.BAD_REQUEST, "요청 정보가 잘못되었습니다"),
 
     /* 409 CONFLICT: 중복 Resource 존재 */
     DUPLICATE_USERID(HttpStatus.CONFLICT, "존재하는 아이디입니다"),
 
-    /* 500  */
+    /* 500 INTERNAL_SERVER_ERROR: 지정하지 않은 오류 발생 */
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 오류가 발생했습니다");
 
     private final HttpStatus httpStatus;
