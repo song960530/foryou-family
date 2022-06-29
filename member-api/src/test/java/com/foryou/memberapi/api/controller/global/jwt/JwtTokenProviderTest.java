f77bacc88d00c86e5d2b2d83c9f54e8080888f3d
송문준 <song960530@naver.com>
2022-06-29 09:47:51 +0900

    [#23] refactor: JWT 검증 로직, Security 제거

--- member-api/src/test/java/com/foryou/memberapi/api/controller/global/jwt/JwtTokenProviderTest.java
+++ member-api/src/test/java/com/foryou/memberapi/api/controller/global/jwt/JwtTokenProviderTest.java
@@ -4,46 +4,31 @@ import com.foryou.memberapi.api.dto.request.JoinReqDto;
 import com.foryou.memberapi.api.entity.Member;
 import com.foryou.memberapi.api.entity.Role;
 import com.foryou.memberapi.api.enums.MemberRole;
-import com.foryou.memberapi.global.constants.Constants;
-import com.foryou.memberapi.global.error.CustomException;
-import com.foryou.memberapi.global.error.ErrorCode;
 import com.foryou.memberapi.global.jwt.JwtTokenProvider;
 import com.foryou.memberapi.global.properties.JwtProperties;
-import com.foryou.memberapi.global.security.UserAdapter;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doReturn;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(MockitoExtension.class)
 class JwtTokenProviderTest {
+
     @InjectMocks
     private JwtTokenProvider jwtTokenProvider;
-
     @Spy
     private JwtProperties jwtProperties;
-    @Mock
-    private UserDetailsService userDetailsService;
-    @Mock
-    private HttpServletRequest request;
 
     private Member member;
 
@@ -110,116 +95,5 @@ class JwtTokenProviderTest {
 
         // then
         assertNotNull(token);
-    }
-
-    @Test
-    @DisplayName("ServletRequest Authorization헤더값이 null일때 빈문자열 리턴")
-    public void authorizationValueIsNull() throws Exception {
-        // given
-        doReturn(null).when(request).getHeader("Authorization");
-
-        // when
-        String token = jwtTokenProvider.extractToken(request);
-
-        // then
-        Assertions.assertEquals(Constants.DEFAULT_TOKEN_VALUE, token);
-    }
-
-    @Test
-    @DisplayName("ServletRequest Authorization헤더값에 토큰이 있으면 토큰만 리턴")
-    public void authorizationValueIsNotNull() throws Exception {
-        // given
-        String authorizationHeaderValue = "Bearer This is Test Token";
-
-        doReturn(authorizationHeaderValue).when(request).getHeader("Authorization");
-
-        // when
-        String resolveToken = jwtTokenProvider.extractToken(request);
-
-        // then
-        assertEquals(authorizationHeaderValue.replaceAll("^(?i)Bearer( )*", ""), resolveToken);
-    }
-
-    @Test
-    @DisplayName("Authorization헤더값 형태가 옳지 않을경우 오류 발생")
-    public void notValidHeaderForm() throws Exception {
-        // given
-        String authorizationHeaderValue = "Bearer123 This is Test Token";
-
-        doReturn(authorizationHeaderValue).when(request).getHeader("Authorization");
-
-        // when
-        CustomException customException = assertThrows(CustomException.class, () -> {
-            jwtTokenProvider.extractToken(request);
-        });
-
-        // then
-        assertEquals(ErrorCode.NOT_VALID_TOKEN_FORM, customException.getErrorCode());
-        assertEquals(HttpStatus.BAD_REQUEST, customException.getErrorCode().getHttpStatus());
-    }
-
-    @Test
-    @DisplayName("JWT 토큰을 파싱하여 Subject를 추출한다")
-    public void passTokenExpire() throws Exception {
-        // given
-        String memberId = "test1234";
-        List<Role> roles = Arrays.stream(MemberRole.values()).map(Role::new).collect(Collectors.toList());
-        String accessToken = jwtTokenProvider.createAccessToken(memberId, roles);
-
-        // when
-        String reuslt = jwtTokenProvider.extractSubject(accessToken);
-
-        // then
-        assertEquals("test1234", reuslt);
-    }
-
-    @Test
-    @DisplayName("토큰 만료일이 지나서 ErrorCode.EXPIRED_TOKEN 발생")
-    public void expiredToken() throws Exception {
-        // given
-        String memberId = "test1234";
-        List<Role> roles = Arrays.stream(MemberRole.values()).map(Role::new).collect(Collectors.toList());
-        ReflectionTestUtils.setField(jwtProperties, "accessValidTime", 0);
-        String accessToken = jwtTokenProvider.createAccessToken(memberId, roles);
-
-        // when
-        CustomException customException = assertThrows(CustomException.class, () -> {
-            jwtTokenProvider.extractSubject(accessToken);
-        });
-
-        // then
-        assertEquals(ErrorCode.EXPIRED_TOKEN, customException.getErrorCode());
-        assertEquals(ErrorCode.EXPIRED_TOKEN.getHttpStatus(), customException.getErrorCode().getHttpStatus());
-    }
-
-    @Test
-    @DisplayName("Header로 넘어온 토큰의 Form은 맞으나 내부 값 검증이 실패했을 때 오류 발생")
-    public void failValidTokenValue() throws Exception {
-        // given
-        String failToken = "header.payload.sign";
-
-        // when
-        CustomException customException = assertThrows(CustomException.class, () -> {
-            jwtTokenProvider.extractSubject(failToken);
-        });
-
-        // then
-        assertEquals(ErrorCode.NOT_VALID_TOKEN_VALUE, customException.getErrorCode());
-        assertEquals(ErrorCode.NOT_VALID_TOKEN_VALUE.getHttpStatus(), customException.getErrorCode().getHttpStatus());
-    }
-
-    @Test
-    @DisplayName("Authentication 객체 생성 성공")
-    public void successCreateAuthentication() throws Exception {
-        // given
-        UserAdapter userAdapter = new UserAdapter(member);
-
-        doReturn(userAdapter).when(userDetailsService).loadUserByUsername(anyString());
-
-        // when
-        Authentication result = jwtTokenProvider.createAuthentication("test123");
-
-        // then
-        assertTrue(result.getPrincipal().equals(userAdapter));
     }
 }
