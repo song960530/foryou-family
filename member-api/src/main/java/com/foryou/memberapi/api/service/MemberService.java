4cfe293af6782b49b535872be5f06bba8e674065
송문준 <song960530@naver.com>
2022-07-01 13:51:47 +0900

    [#26] feat,test: 토큰정보 저장 및 재발급 로직 구현

--- member-api/src/main/java/com/foryou/memberapi/api/service/MemberService.java
+++ member-api/src/main/java/com/foryou/memberapi/api/service/MemberService.java
@@ -3,10 +3,13 @@ package com.foryou.memberapi.api.service;
 import com.foryou.memberapi.api.dto.request.JoinReqDto;
 import com.foryou.memberapi.api.dto.request.LoginReqDto;
 import com.foryou.memberapi.api.dto.response.LoginResDto;
+import com.foryou.memberapi.api.dto.response.TokenResDto;
 
 public interface MemberService {
 
     Long join(JoinReqDto joinDto);
 
     LoginResDto login(LoginReqDto loginReqDto);
+
+    TokenResDto reCreateToken(String accessToken, String refreshToken);
 }
--- member-api/src/test/java/com/foryou/memberapi/api/controller/global/jwt/JwtTokenProviderTest.java
+++ member-api/src/test/java/com/foryou/memberapi/api/controller/global/jwt/JwtTokenProviderTest.java
@@ -4,6 +4,8 @@ import com.foryou.memberapi.api.dto.request.JoinReqDto;
 import com.foryou.memberapi.api.entity.Member;
 import com.foryou.memberapi.api.entity.Role;
 import com.foryou.memberapi.api.enums.MemberRole;
+import com.foryou.memberapi.global.error.CustomException;
+import com.foryou.memberapi.global.error.ErrorCode;
 import com.foryou.memberapi.global.jwt.JwtTokenProvider;
 import com.foryou.memberapi.global.properties.JwtProperties;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,14 +15,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class JwtTokenProviderTest {
@@ -95,5 +97,66 @@ class JwtTokenProviderTest {
 
         // then
         assertNotNull(token);
+    }
+
+    @Test
+    @DisplayName("subject 추출 성공")
+    public void successExtractSubject() throws Exception {
+        // given
+        String memberId = "test1234";
+        String refreshToken = jwtTokenProvider.createRefreshToken(memberId);
+
+        // when
+        String subject = jwtTokenProvider.extractSubject(refreshToken);
+
+        // then
+        assertEquals(memberId, subject);
+    }
+
+    @Test
+    @DisplayName("subject 추출 중 오류 발생")
+    public void exceptionExtractSubject() throws Exception {
+        // given
+
+        // when
+        CustomException customException = assertThrows(CustomException.class, () -> {
+            jwtTokenProvider.extractSubject("test");
+        });
+
+        // then
+        assertEquals(ErrorCode.NOT_VALID_TOKEN_VALUE, customException.getErrorCode());
+        assertEquals(HttpStatus.BAD_REQUEST, customException.getErrorCode().getHttpStatus());
+    }
+
+    @Test
+    @DisplayName("subject 추출 중 만료된 토큰예외 처리")
+    public void expiredToken() throws Exception {
+        // given
+        String memberId = "test1234";
+        ReflectionTestUtils.setField(jwtProperties, "refreshValidTime", 0);
+        String refreshToken = jwtTokenProvider.createRefreshToken(memberId);
+
+        // when
+        CustomException customException = assertThrows(CustomException.class, () -> {
+            jwtTokenProvider.extractSubject(refreshToken);
+        });
+
+        // then
+        assertEquals(ErrorCode.EXPIRED_TOKEN, customException.getErrorCode());
+        assertEquals(HttpStatus.UNAUTHORIZED, customException.getErrorCode().getHttpStatus());
+    }
+
+    @Test
+    @DisplayName("토큰 패턴 확인")
+    public void matchedTokenPattern() throws Exception {
+        // given
+        String memberId = "test1234";
+        String refreshToken = jwtTokenProvider.createRefreshToken(memberId);
+
+        // when
+        boolean result = jwtTokenProvider.isMatchedPrefix("Bearer " + refreshToken);
+
+        // then
+        assertTrue(result);
     }
 }
