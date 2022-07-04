558dfe08e8d36b6d395a92243258e4bf36de3f78
송문준 <song960530@naver.com>
2022-07-04 21:04:48 +0900

    [#34] refactor: 안쓰는 함수 제거

--- party-api/src/main/java/com/foryou/partyapi/api/repository/PartyRepository.java
+++ party-api/src/main/java/com/foryou/partyapi/api/repository/PartyRepository.java
@@ -5,11 +5,7 @@ import com.foryou.partyapi.api.enums.OttType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
-
 public interface PartyRepository extends JpaRepository<Party, Long> {
-    Optional<Party> findByMemberId(String memberId);
-
     @Query("select count(p) > 0 " +
             "from Party p " +
             "where p.memberId=:memberId " +
