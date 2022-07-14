7eb0e50db369778c4cd7140d7c82d45f06dc0cd8
송문준 <song960530@naver.com>
2022-07-14 14:19:47 +0900

    [#54] refactor: 접근제어자 수정

--- matching-service/src/main/java/com/foryou/matchingservice/api/queue/FirstQueue.java
+++ matching-service/src/main/java/com/foryou/matchingservice/api/queue/FirstQueue.java
@@ -3,9 +3,9 @@ package com.foryou.matchingservice.api.queue;
 import com.foryou.matchingservice.api.dto.response.Response;
 
 public interface FirstQueue {
-    public void offerMember(Long no);
+    void offerMember(Long no);
 
-    public void offerOwner(Long no);
+    void offerOwner(Long no);
 
-    public Response pollQueues();
+    Response pollQueues();
 }
