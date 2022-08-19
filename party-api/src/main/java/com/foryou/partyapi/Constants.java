dbf031c66e96d2f2538d3629400f16410af1ae96
송문준 <song960530@naver.com>
2022-08-19 14:13:44 +0900

    [#69] feat: kafka Config 추가

--- party-api/src/main/java/com/foryou/partyapi/Constants.java
+++ party-api/src/main/java/com/foryou/partyapi/Constants.java
@@ -2,7 +2,9 @@ package com.foryou.partyapi;
 
 public final class Constants {
     public static final String KAFKA_TOPIC_PARTY = "party";
-    public static final String KAFKA_GROPU_ID_PARTY = "party_group";
+    public static final String KAFKA_TOPIC_MATCH_RESULT = "match_result";
+    public static final String KAFKA_GROPU_ID_MATCH_RESULT = "match_result_group";
+
     public static final String KAFKA_AUTO_OFFSET_RESET_EARLIEST = "earliest";
 
     public static final String REQUEST_HEADER_MEMBER_ID = "MEMBER-ID";
