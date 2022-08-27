6bff948768fd970f843e7b1908ebb5cb94ef2430
송문준 <song960530@naver.com>
2022-08-27 14:49:21 +0900

    [#75] fix: Controller -> RestController 변경

--- billing-api/src/main/java/com/foryou/billingapi/api/controller/PaymentController.java
+++ billing-api/src/main/java/com/foryou/billingapi/api/controller/PaymentController.java
@@ -8,16 +8,16 @@ import com.siot.IamportRestClient.response.IamportResponse;
 import com.siot.IamportRestClient.response.Payment;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.math.BigDecimal;
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class PaymentController {
     private final PaymentService paymentService;
