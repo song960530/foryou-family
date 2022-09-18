831c924022ee66c886a35cd9bd4a836d5bb5a5d6
송문준 <song960530@naver.com>
2022-09-18 23:05:35 +0900

    [#79] feat: CANCEL 상태 추가

--- matching-service/src/main/java/com/foryou/matchingservice/api/enums/StatusType.java
+++ matching-service/src/main/java/com/foryou/matchingservice/api/enums/StatusType.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum StatusType {
-    WAIT("WAIT"), START("START"), COMPLETE("COMPLETE"), ALL_COMPLETE("ALL_COMPLETE");
+    WAIT("WAIT"), START("START"), COMPLETE("COMPLETE"), ALL_COMPLETE("ALL_COMPLETE"), CANCEL("CANCEL");
 
     private final String value;
 }
