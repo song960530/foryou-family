24585c947443bbb68dd219a93cace2af413b4190
송문준 <song960530@naver.com>
2022-09-27 19:15:24 +0900

    #[79] test: 메인 테스트 클래스 @SpringBootTest 추가

--- matching-service/src/test/java/com/foryou/matchingservice/MatchingServiceApplicationTests.java
+++ matching-service/src/test/java/com/foryou/matchingservice/MatchingServiceApplicationTests.java
@@ -1,7 +1,9 @@
 package com.foryou.matchingservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class MatchingServiceApplicationTests {
 
     @Test
