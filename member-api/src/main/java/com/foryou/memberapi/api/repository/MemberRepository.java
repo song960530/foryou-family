8d93d3b68f35909bfc38989d0703220db4f28f54
송문준 <song960530@naver.com>
2022-10-11 14:52:42 +0900

    [#92] fix: roles 삭제

--- member-api/src/main/java/com/foryou/memberapi/api/repository/MemberRepository.java
+++ member-api/src/main/java/com/foryou/memberapi/api/repository/MemberRepository.java
@@ -1,7 +1,6 @@
 package com.foryou.memberapi.api.repository;
 
 import com.foryou.memberapi.api.entity.Member;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +11,5 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByMemberId(String memberId);
 
-    @EntityGraph(attributePaths = {"roles"})
     Optional<Member> findByMemberId(String memberId);
 }
