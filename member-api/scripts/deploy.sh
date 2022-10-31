24623bc41c28e183507cd3d1a731790e6a7944e5
송문준 <song960530@naver.com>
2022-10-31 11:38:05 +0900

    [#99] fix: add java -jar option

--- member-api/scripts/deploy.sh
+++ member-api/scripts/deploy.sh
@@ -22,4 +22,4 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log &
+nohup java -jar $DEPLOY_JAR --spring.profiles.active=prod >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log &
