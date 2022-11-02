bb403f79af47d1d62768b2f3e42a920eefc53fa7
송문준 <song960530@naver.com>
2022-11-02 17:01:11 +0900

    [#99] test: ActiveProfiles 추가

--- billing-api/src/test/java/com/foryou/billingapi/BillingApiApplicationTests.java
+++ billing-api/src/test/java/com/foryou/billingapi/BillingApiApplicationTests.java
@@ -2,12 +2,14 @@ package com.foryou.billingapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("prod")
 class BillingApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
--- party-api/src/test/java/com/foryou/partyapi/PartyApiApplicationTests.java
+++ party-api/src/test/java/com/foryou/partyapi/PartyApiApplicationTests.java
@@ -2,12 +2,14 @@ package com.foryou.partyapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("prod")
 class PartyApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
